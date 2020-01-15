b3937680f09e92641172c2173410effa6d21194f
Safnaj <safnaj99@gmail.com>
2020-01-16 00:25:53 +0530

    New Routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -25,6 +25,7 @@ const routes: Routes = [
   { path: 'products', component: ProductsComponent},
   { path: 'shopping-cart', component: ShoppingCartComponent},
   { path: 'check-out', component: CheckOutComponent},
+  { path: 'my/orders', component: MyOrdersComponent},
   { path: 'order-success', component: OrderSuccessComponent},
   { path: 'login', component: LoginComponent},
   { path: 'admin/products', component: AdminProductsComponent},
