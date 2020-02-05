e9a7f55ef9fcebd6c05803f287b18d4f216e33c4
Safnaj <safnaj99@gmail.com>
2020-02-05 22:36:16 +0530

    Sign-in with Google

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
 
-  constructor() { }
+  constructor(private aFauth: AngularFireAuth) { }
 
-  ngOnInit() {
+  //Removed onInit Method
+
+  login() {
+    console.log("Hello");
+    this.aFauth.auth.signInWithRedirect(new firebase.auth.GoogleAuthProvider());
   }
 
 }
