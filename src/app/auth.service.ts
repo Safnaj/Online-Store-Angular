3b32116fdb0cc589d3c3413867e8f892bf39336a
Safnaj <safnaj99@gmail.com>
2020-02-21 00:32:06 +0530

    Redirect After Login

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { AngularFireAuth } from '@angular/fire/auth';
 import * as firebase from 'firebase';
 import { Observable } from 'rxjs';
@@ -10,11 +11,16 @@ export class AuthService {
 
   user$: Observable<firebase.User>;
 
-  constructor(private aFauth: AngularFireAuth) {
+  constructor(private aFauth: AngularFireAuth, private route: ActivatedRoute) {
     this.user$ = aFauth.authState;    
    }
 
   login(){
+    //Stores URL in Local Storage to Redirect Back after logged in
+    //Working with App Component
+    let returnUrl = this.route.snapshot.queryParamMap.get('returnUrl') || '/';
+    localStorage.setItem('returnUrl', returnUrl);
+
     this.aFauth.auth.signInWithRedirect(new firebase.auth.GoogleAuthProvider());
   }
 
