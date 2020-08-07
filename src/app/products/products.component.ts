fbde51a06366376786dfa141374f338aca52f2d3
Safnaj <safnaj99@gmail.com>
2020-08-08 02:16:21 +0530

    Product Cart Quantity

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,33 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ProductService } from '../product.service';
 import { ActivatedRoute } from '@angular/router';
 import { Product } from '../models/product';
+import { ShoppingCartService } from '../shopping-cart.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.css']
 })
-export class ProductsComponent implements OnInit {
+export class ProductsComponent implements OnInit, OnDestroy {
   products: Product[] = [];
   filteredProducts: Product[] = [];  
   category: string;
+  cart: any;
+  subscription: Subscription;
 
-  constructor(route: ActivatedRoute, productService: ProductService) {
-      productService.getAll().subscribe(products => {
-        this.products = products;
+  constructor(route: ActivatedRoute, productService: ProductService, private shoppingCartService: ShoppingCartService) {
+    productService.getAll().subscribe(products => {
+      this.products = products;
 
-        route.queryParamMap.subscribe(params => {
-          this.category = params.get('category');
-          this.filteredProducts = (this.category) ?
-            this.products.filter(p => p.category === this.category) :
-            this.products;
-        });
-        
-      });     
+      route.queryParamMap.subscribe(params => {
+        this.category = params.get('category');
+        this.filteredProducts = (this.category) ?
+          this.products.filter(p => p.category === this.category) :
+          this.products;
+      });
+      
+    });     
+
   }
 
-  ngOnInit() {
+  async ngOnInit() {
+    this.subscription = (await this.shoppingCartService.getCart())
+    .subscribe(cart => this.cart = cart);
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 
 }
--- src/app/shopping-cart.service.ts
+++ src/app/shopping-cart.service.ts
@@ -1,5 +1,8 @@
 import { Injectable } from '@angular/core';
 import { AngularFireDatabase } from '@angular/fire/database';
+import { Product } from './models/product';
+import { take } from 'rxjs/operators';
+
 
 @Injectable({
   providedIn: 'root'
@@ -8,10 +11,43 @@ export class ShoppingCartService {
 
   constructor(private db: AngularFireDatabase) { }
 
-  create(){
+  private create(){
     return this.db.list('/shopping-carts').push({
       dateCreated: new Date().getTime()
     });
   }
 
+  async getCart(){
+    let cartId = await this.getOrCreateCartId();
+    return this.db.object('/shopping-carts/'+cartId).valueChanges();
+  }
+
+  private async getOrCreateCartId(): Promise<string> {
+    let cartId = localStorage.getItem('cartId');
+    if(cartId) return cartId;
+
+    let result = await this.create();
+    localStorage.setItem('cartId', result.key);
+    return result.key;
+  }
+
+  private getItem(cartId: string, productId: string){
+    return this.db.object('/shopping-carts/' + cartId + '/items/' + productId);
+  }
+
+  async addToCart(product : Product){
+    let cartId = await this.getOrCreateCartId();
+    let item$ = this.getItem(cartId,product.key);
+    
+    item$.snapshotChanges().pipe(take(1)).subscribe((item:any) =>  {
+      if (item.key != null) {
+        item$.update( {quantity:( item.payload.val().quantity || 0) + 1}); 
+      }
+      else{
+         item$.set( {product:product, quantity:1}); 
+        }
+    }); 
+  }
+
+  
 }
